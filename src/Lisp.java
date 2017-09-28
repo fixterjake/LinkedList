a0bf380c74a1400a3e35f6ee55d0d81b3287c4a1
Jacob Boyles <jacob.boyles.16@cnu.edu>
2017-09-28 16:41:25 -0400

    Initialized implementation using the api

--- src/Lisp.java
+++ src/Lisp.java
@@ -1,9 +1,49 @@
 /**
- * Default comment, change this
  * @author Jacob Boyles
  * @version 1.0
  */
+public class Lisp implements LispADT {
 
-public class Lisp {
+    @Override
+    public LispADT add(Object c) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object get() throws RuntimeException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public LispADT moveTo(Integer p) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public LispADT next() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public LispADT prev() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public LispADT remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Integer size() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
